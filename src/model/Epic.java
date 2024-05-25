febd8f8561eb5d546b8bd620db3831ff62f452a1
RBiderman <biderman@vtb.ru>
2024-05-25 13:26:47 +0300

    Спринт 8. Техническое задание. CodeStyle

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -19,6 +19,7 @@ public class Epic extends Task {
     public LocalDateTime getEndTime() {
         return endTime;
     }
+
     public void setEndTime(LocalDateTime endTime) {
         this.endTime = endTime;
     }
